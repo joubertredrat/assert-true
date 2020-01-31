eba4df5063a56032142d520fd5dc76410c0e00ba
Joubert RedRat <eu+github@redrat.com.br>
2020-01-31 14:55:46 -0300

    Create initial code to be happy

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# assert-true
+# Assert true
+
 A lot of ways to you set your assert as true
+
+Sometimes we need to be sure that `assertTrue` going to really assert `true`.
+
+Then, here you're going to found a lot of ways to assert true in your unit tests for lot of purposes and be happy ~~while you don't write real unit test to assert~~
+
+Is obvious that this is a healthy joke, but if you're intolerant to jokes, move out, I don't need you here.
+
+#### Dependencies
+
+```
+php >= 7.2
+```
+
+#### Run
+
+```bash
+composer install
+composer run tests
+```
+
+#### Author
+
+The polemic me
---
+++ tests/Unit/AssertTrueTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * AssertTrueTest
+ *
+ * @package Tests\Unit
+ */
+class AssertTrueTest extends TestCase
+{
+    /**
+     * Let's test the obvious
+     *
+     * @return void
+     */
+    public function testObviousTrue(): void
+    {
+        self::assertTrue(true);
+    }
+
+    /**
+     * Yeah, isn't obvious, a lot of people don't know how to negation works
+     *
+     * @return void
+     */
+    public function testMoreOrLessObvious(): void
+    {
+        self::assertTrue(!false);
+    }
+
+    /**
+     * Complex, but is sufficient to deploy in friday and move this problem to future
+     *
+     * @return void
+     */
+    public function testPleaseINeedDoDeployItInFriday(): void
+    {
+        self::assertTrue(!!!((1 === 2) !== false));
+    }
+}
